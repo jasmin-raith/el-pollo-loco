dd4d24562b8cc360b139743e24a620ce6042626f
Jasmin Raith <jasmin-raith@outlook.com>
2023-08-28 16:59:06 +0200

    first class implemented

--- js/game.js
+++ js/game.js
@@ -1,13 +1,11 @@
 let canvas;
 let ctx;
-let chracter = new Image();
+let chracter = new MovableObject();
 
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
-    chracter.src = '../img/2_character_pepe/2_walk/W-21.png';
-
-    ctx.drawImage(chracter, 20, 20, 50, 150);
+    console.log('My character is', chracter);
 }
---
+++ models/movable-object.class.js
@@ -0,0 +1,10 @@
+class MovableObject {
+    x = 120;
+    y = 400;
+    img;
+
+
+    moveRight() {
+
+    }
+}
