923960073fb78425a99cfe12f8155e6822e1a02a
Jasmin Raith <jasmin-raith@outlook.com>
2023-08-29 12:28:31 +0200

    show character

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,7 +1,9 @@
 class MovableObject {
     x = 120;
-    y = 400;
+    y = 250;
     img;
+    height = 150;
+    width = 100;
 
 
     loadImage(path) {
--- models/world.class.js
+++ models/world.class.js
@@ -5,10 +5,16 @@ class World {
         new Chicken(),
         new Chicken()
     ];
+    ctx;
+
+    constructor() {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
+    }
 
 
 
     draw() {
-
+        this.ctx.drawImage(this.chracter.img, this.chracter.x, this.chracter.y, this.chracter.width, this.chracter.height);
     }
 }
