87b5998353422c265b5abcecc77349ee7333e1fd
Jasmin Raith <jasmin-raith@outlook.com>
2023-08-30 11:36:51 +0200

    show chicken and added requestAnimationFrame

--- models/world.class.js
+++ models/world.class.js
@@ -5,16 +5,30 @@ class World {
         new Chicken(),
         new Chicken()
     ];
+    canvas;
     ctx;
 
     constructor() {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.chracter.img, this.chracter.x, this.chracter.y, this.chracter.width, this.chracter.height);
+        this.enemies.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
+        });
+
+
+        // draw() wird immer wieder aufgerufen
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw();
+        });
     }
 }
