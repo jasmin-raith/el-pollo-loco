303822cc5d654096b98eca955c29f8a9c2602226
Jasmin Raith <jasmin-raith@outlook.com>
2023-09-14 13:23:09 +0200

    move clouds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,5 +7,13 @@ class Cloud extends MovableObject {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
         this.x = Math.random() * 500;  // Zahl zwischen 0 und 500
+        this.animate();
+    }
+
+
+    animate() {
+        setInterval(() => {    // 60 mal pro Sekunde werden die Wolken um 0.15 Pixel nach links verschoben
+            this.x -= 0.15;          
+        }, 1000 / 60);
     }
 }
