8b2114cc8fbe473c5116333d5989fb17df7a47f6
Jasmin Raith <jasmin-raith@outlook.com>
2023-09-14 15:00:21 +0200

    character animated

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -20,7 +20,7 @@ class MovableObject {
         arr.forEach((path) => {
             let img = new Image();
             img.src = path;
-            this.imageCache[path] = path;
+            this.imageCache[path] = img;
         });
     }
 
