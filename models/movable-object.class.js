83f4a1812651fe3762ff84496af5af88290934fa
Jasmin Raith <jasmin-raith@outlook.com>
2023-09-14 14:21:07 +0200

    added image cache

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,11 +4,24 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
 
     loadImage(path) {
         this.img = new Image();  // this.img = document.getElementById('image')  <img id="image" src>
         this.img.src = path;
+    }
+     
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
     }
 
     moveRight() {
