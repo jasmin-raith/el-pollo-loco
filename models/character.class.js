4b79fcb2ee5b0814774e75112d15d0c4908ce86b
Jasmin Raith <jasmin-raith@outlook.com>
2023-09-14 12:41:24 +0200

    objects scaled

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,7 @@
 class Character extends MovableObject {
-
+    y = 140;
+    height = 300;
+    width = 140;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
