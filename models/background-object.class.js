aa40c4e9fcbb4a052b09754b076ee171b60ad0fc
Jasmin Raith <jasmin-raith@outlook.com>
2023-09-14 12:20:37 +0200

    added more background objects

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,10 +1,10 @@
 class BackgroundObject extends MovableObject {
     width = 720;
     height = 480;
-    
-    constructor(imagePath, x, y) {
+
+    constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
-        this.y = y;
+        this.y = 480 - this.height;
     }
 }
--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,10 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 0)
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
     ];
     canvas;
     ctx;
