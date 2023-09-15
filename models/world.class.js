ffb18d96f7a39c65e2d662ba5dcb454b6d208d05
Jasmin Raith <jasmin-raith@outlook.com>
2023-09-15 13:35:46 +0200

    move camera view

--- models/world.class.js
+++ models/world.class.js
@@ -17,6 +17,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
 
     constructor(canvas, keyboard) {
@@ -35,10 +36,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.chracter);
         this.addObjectsToMap(this.clouds);
         this.addObjectsToMap(this.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
 
 
 
