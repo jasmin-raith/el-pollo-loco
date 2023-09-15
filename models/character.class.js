7b01e6f1da87ea26b1d1c0a0180d06fec6f950fa
Jasmin Raith <jasmin-raith@outlook.com>
2023-09-15 12:00:03 +0200

    move character

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
     y = 140;
     height = 300;
     width = 140;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -20,17 +21,25 @@ class Character extends MovableObject {
     }
 
     animate() {
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT)
+            this.x += this.speed;
+
+            if (this.world.keyboard.LEFT)
+            this.x -= this.speed;
+        }, 1000 / 60);
+
         setInterval(() => {
 
-            if (this.world.keyboard.RIGHT) {
-
-                let i = this.currentImage % this.IMAGES_WALKING.length;  // Beispiel: let i = 0 % 6 => 0, Rest 5  // let i = 1 % 6 => 1, Rest 4
-                // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5 ....
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                // Walk animation
+                let i = this.currentImage % this.IMAGES_WALKING.length;  // Beispiel: let i = 0 % 6 => 0, Rest 5  // let i = 1 % 6 => 1, Rest 4  // i = 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5 ....
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++
             }
-        }, 100);
+        }, 50);
     }
 
     jump(){
